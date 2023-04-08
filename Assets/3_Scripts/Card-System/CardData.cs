584bd337f6991ab37db88cf609b5dc2f1d9094fc
Oguzhan Delibas <oguzhan.delibass@gmail.com>
2023-04-08 21:31:16 +0300

    +add: font

--- Assets/3_Scripts/Card-System/CardData.cs
+++ Assets/3_Scripts/Card-System/CardData.cs
@@ -6,17 +6,26 @@ using UnityEngine.UI;
 [CreateAssetMenu(menuName = "ScriptableObjects/Card/CardData")]
 public class CardData : ScriptableObject
 {
+    [Header("Is it for swiping left?")]
     public bool forLeft;
+
+    [Header("Necessary task?")]
     public bool isRequired;
 
+    [Header("Q&A")]
     public string cardText;
     public string[] answers;
 
+    [Header("Indicator Values")]
     public int academicSuccess;
     public int network;
     public int experience;
     public int selfImprovment;
 
+    [Header("How many days will it cost?")]
+    public int dayCost;
+
+    [Header("Card Design")]
     public Sprite characterImage;
     public Color backgroundColor;
 }
