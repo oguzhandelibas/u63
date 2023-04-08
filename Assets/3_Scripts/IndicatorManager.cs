dadc437ba61af1e7b0c9b0c8bb2288db602e7341
berketabak <berketabak1@gmail.com>
2023-04-08 15:47:12 +0300

    Script Update

--- Assets/3_Scripts/IndicatorManager.cs
+++ Assets/3_Scripts/IndicatorManager.cs
@@ -5,60 +5,33 @@ using UnityEngine.UI;
 
 public class IndicatorManager : MonoBehaviour
 {
-    public Slider[] deneme;
-    public Slider sliderOne, sliderTwo, sliderThree, sliderFour;
+    public Image academicImage, networkImage, experienceImage, selfImprovementImage;
+    
     public int degisimDegeri;
     // Start is called before the first frame update
     void Start()
     {
-        deneme = FindObjectsOfType<Slider>();
-         degisimDegeri = 10;
+        
+         
     }
 
     public void Change(int academic,int network, int experience,int selfImprovement)
     {
         // akademik baþarý-network-experience-selfImprovement
-        sliderOne.value += academic;
-        sliderTwo.value += network;
-        sliderThree.value += experience;
-        sliderFour.value += selfImprovement;
+        academicImage.fillAmount += (academic/100);
+        networkImage.fillAmount+= (network/100);
+        experienceImage.fillAmount += (experience/100);
+        selfImprovementImage.fillAmount += (selfImprovement/100);
 
+
+        if( academicImage.fillAmount==0 || networkImage.fillAmount==0 || experienceImage.fillAmount==0 || selfImprovementImage.fillAmount==0 )
+        {
+            Debug.Log("lose");
+        }
         //LoseControl();
         // UpdateIndicators();
     }
 
 
-    public void Arttirma()
-    {
-
-        sliderOne.value += degisimDegeri;
-        sliderTwo.value += degisimDegeri;
-        sliderThree.value -= degisimDegeri;
-        sliderFour.value -= degisimDegeri;
-
-    }
-
-    public void Azaltma()
-    {
-
-        sliderOne.value -= degisimDegeri;
-        sliderTwo.value -= degisimDegeri;
-        sliderThree.value += degisimDegeri;
-        sliderFour.value += degisimDegeri;
-    }
-
-    /*  public LoseControl()
-      {
-          foreach (var value in deneme)
-          {
-              if (value.value < 0)
-                  return true;
-
-          }  
-
-          //if(FindObjectsOfType<Slider>
-      } */
-    // public UpdateIndicators()
-
-
+ 
 }
