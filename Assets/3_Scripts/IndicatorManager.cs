174b647457ab5acaa4e7e9b176445d3d12ae6bbb
berketabak <berketabak1@gmail.com>
2023-04-08 15:25:13 +0300

    Change Function Added

--- Assets/3_Scripts/IndicatorManager.cs
+++ Assets/3_Scripts/IndicatorManager.cs
@@ -5,12 +5,26 @@ using UnityEngine.UI;
 
 public class IndicatorManager : MonoBehaviour
 {
+    public Slider[] deneme;
     public Slider sliderOne, sliderTwo, sliderThree, sliderFour;
     public int degisimDegeri;
     // Start is called before the first frame update
     void Start()
     {
+        deneme = FindObjectsOfType<Slider>();
          degisimDegeri = 10;
+    }
+
+    public void Change(int academic,int network, int experience,int selfImprovement)
+    {
+        // akademik baþarý-network-experience-selfImprovement
+        sliderOne.value += academic;
+        sliderTwo.value += network;
+        sliderThree.value += experience;
+        sliderFour.value += selfImprovement;
+
+        //LoseControl();
+        // UpdateIndicators();
     }
 
 
@@ -32,6 +46,19 @@ public class IndicatorManager : MonoBehaviour
         sliderThree.value += degisimDegeri;
         sliderFour.value += degisimDegeri;
     }
+
+    /*  public LoseControl()
+      {
+          foreach (var value in deneme)
+          {
+              if (value.value < 0)
+                  return true;
+
+          }  
+
+          //if(FindObjectsOfType<Slider>
+      } */
+    // public UpdateIndicators()
 
 
 }
